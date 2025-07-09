391c0e16ed9bff0ec3b1fb7c9f04031494113165
Gatu-1548 <jorge.alfredo.mamani@gmail.com>
2025-07-09 08:57:56 -0400

    feact users

---
+++ lib/pages/User/UserManagementPage .dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sigfrontend/providers/UserService.dart';
+import 'package:sigfrontend/providers/user_provider.dart';
+
+class UserManagementPage extends StatefulWidget {
+  const UserManagementPage({super.key});
+
+  @override
+  State<UserManagementPage> createState() => _UserManagementPageState();
+}
+
+class _UserManagementPageState extends State<UserManagementPage> {
+  List<Map<String, dynamic>> _users = [];
+  bool _isLoading = true;
+
+  Future<void> loadUsers(UserService service) async {
+    setState(() => _isLoading = true);
+    try {
+      _users = await service.getAllUsers();
+    } catch (e) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text('Error al cargar usuarios: $e')));
+    } finally {
+      setState(() => _isLoading = false);
+    }
+  }
+
+  void _editUser(UserService service, Map<String, dynamic> user) async {
+    final controller = TextEditingController(
+      text: user['phone_number']?.toString() ?? '',
+    );
+    await showDialog(
+      context: context,
+      builder:
+          (_) => AlertDialog(
+            title: const Text('Editar teléfono'),
+            content: TextField(
+              controller: controller,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(labelText: 'Teléfono'),
+            ),
+            actions: [
+              TextButton(
+                onPressed: () async {
+                  await service.updateUser(user['id'], {
+                    'phone_number': int.tryParse(controller.text),
+                  });
+                  Navigator.pop(context);
+                  await loadUsers(service);
+                },
+                child: const Text('Guardar'),
+              ),
+            ],
+          ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    final token =
+        Provider.of<UserProvider>(context, listen: false).accessToken!;
+    final userService = UserService(token);
+    loadUsers(userService);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final userProvider = Provider.of<UserProvider>(context);
+    final userService = UserService(userProvider.accessToken!);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Gestión de usuarios'),
+        backgroundColor: const Color.fromARGB(255, 248, 2, 15),
+      ),
+      body:
+          _isLoading
+              ? const Center(child: CircularProgressIndicator())
+              : RefreshIndicator(
+                onRefresh: () => loadUsers(userService),
+                child: ListView.builder(
+                  padding: const EdgeInsets.all(12),
+                  itemCount: _users.length,
+                  itemBuilder: (_, index) {
+                    final user = _users[index];
+                    return Card(
+                      elevation: 4,
+                      margin: const EdgeInsets.symmetric(vertical: 8),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          backgroundColor:
+                              user['state'] == 'available'
+                                  ? Colors.green
+                                  : Colors.grey,
+                          child: const Icon(Icons.person, color: Colors.white),
+                        ),
+                        title: Text(
+                          user['email'] ?? 'Sin email',
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(height: 4),
+                            Text('Teléfono: ${user['phone_number'] ?? "N/A"}'),
+                            Text(
+                              'Estado: ${user['state']}',
+                              style: TextStyle(
+                                color:
+                                    user['state'] == 'available'
+                                        ? Colors.green
+                                        : Colors.red,
+                              ),
+                            ),
+                          ],
+                        ),
+                        trailing: PopupMenuButton<String>(
+                          onSelected: (value) async {
+                            switch (value) {
+                              case 'activar':
+                                await userService.enableUser(user['id']);
+                                break;
+                              case 'desactivar':
+                                await userService.disableUser(user['id']);
+                                break;
+                              case 'editar':
+                                _editUser(userService, user);
+                                return;
+                              case 'eliminar':
+                                await userService.deleteUser(user['id']);
+                                break;
+                            }
+                            await loadUsers(userService);
+                          },
+                          itemBuilder:
+                              (_) => [
+                                if (user['state'] == 'available')
+                                  const PopupMenuItem(
+                                    value: 'desactivar',
+                                    child: Text('Desactivar'),
+                                  ),
+                                if (user['state'] != 'available')
+                                  const PopupMenuItem(
+                                    value: 'activar',
+                                    child: Text('Activar'),
+                                  ),
+                                const PopupMenuItem(
+                                  value: 'editar',
+                                  child: Text('Editar teléfono'),
+                                ),
+                                const PopupMenuItem(
+                                  value: 'eliminar',
+                                  child: Text('Eliminar'),
+                                ),
+                              ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+    );
+  }
+}
